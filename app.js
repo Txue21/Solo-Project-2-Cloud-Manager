59896c441cc556a81ca973d7de558ac9078ca2a0
Txue21 <xue7@g.clemson.edu>
2026-02-06 22:21:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://tian-games.gamer.gd/api.php';
+const API_URL = 'https://tian-game-vault.com/api.php';
 const gameForm = document.getElementById('game-form');
 let allGames = [];
 let currentPage = 1;
